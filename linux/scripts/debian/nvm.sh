3d6bfc09e362f78ac77b027710803efe4719481b
Akarapol Kasivittayanun <akarapol@gmail.com>
2023-07-20 22:55:31 +0700

    feat: add script for install deno and bun

--- linux/scripts/debian/nvm.sh
+++ linux/scripts/debian/nvm.sh
@@ -13,7 +13,7 @@ install_nvm() {
   print_header "Install nvm"
 
   if ! [ -d "${HOME}/.nvm" ]; then
-    curl -s https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash -s
+    curl -fsSL https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 
     print_header "Install node ${NODE_VERSION}"
 
@@ -30,11 +30,54 @@ install_nvm() {
     npm --version
     yarn --version
 
-    printf "\n%s\n%s\n%s" \
-      "export NVM_DIR='$HOME/.nvm'" \
-      "[ -s '$NVM_DIR/nvm.sh' ] && \. '$NVM_DIR/nvm.sh'  # This loads nvm" \
-      "[ -s '$NVM_DIR/bash_completion' ] && \. '$NVM_DIR/bash_completion'  # This loads nvm bash_completion" |
+    printf "\n%s\n%s\n%s\n" \
+      "export NVM_DIR=\"\$HOME/.nvm\"" \
+      "[ -s \"\$NVM_DIR/nvm.sh\" ] && \. \"\$NVM_DIR/nvm.sh\"  # This loads nvm" \
+      "[ -s \"\$NVM_DIR/bash_completion\" ] && \. \"\$NVM_DIR/bash_completion\"  # This loads nvm bash_completion" |
       tee -a ~/.zshrc >/dev/null
+  fi
+}
 
+install_deno() {
+  clear_screen
+
+  print_header "Install deno"
+
+  if ! [ -d "${HOME}/.deno" ]; then
+
+    export DENO_DIR="$HOME/.deno"
+    export PATH="$DENO_DIR/bin:$PATH"
+
+    curl -fsSL https://deno.land/x/install/install.sh | bash
+
+    printf "\n%s\n%s\n" \
+      "export DENO_DIR=\"\$HOME/.deno\"" \
+      "export PATH=\"\$DENO_DIR/bin:\$PATH\"" |
+      tee -a ~/.zshrc ~/.bashrc >/dev/null
+
+    #smoke test
+    deno --version
+  fi
+}
+
+install_bun() {
+  clear_screen
+
+  print_header "Install bun"
+
+  if ! [ -d "${HOME}/.bun" ]; then
+    
+    export BUN_DIR="$HOME/.bun"
+    export PATH="$BUN_DIR/bin:$PATH"
+
+    curl -fsSL https://bun.sh/install | bash
+
+    printf "\n%s\n%s\n" \
+      "export BUN_DIR=\"\$HOME/.bun\"" \
+      "export PATH=\"\$BUN_DIR/bin:\$PATH\"" |
+      tee -a ~/.zshrc ~/.bashrc >/dev/null
+
+    #smoke test
+    bun --version
   fi
 }
